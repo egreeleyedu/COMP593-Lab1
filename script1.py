40fbef71b85e20b485cc9d312b012cbaf2036a7a
UrTexts <greeleyethan@gmail.com>
2026-05-13 17:08:13 -0400

    Modified both script files

--- script1.py
+++ script1.py
@@ -1,3 +1,3 @@
 # This is fantasic programming advice, it reminds me of Steve Jobs
-
+print("Beautiful is better than ugly.")
 print("Simple is better than complex.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# Now is better than never. True for all things.
 print("Readability counts.")
 print("Now is better than never.")
